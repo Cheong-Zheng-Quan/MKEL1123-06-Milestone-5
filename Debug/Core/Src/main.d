58b6cc54208984eceaf035bd292fa8d0a86e9749
Cheong-Zheng-Quan <82043667+Cheong-Zheng-Quan@users.noreply.github.com>
2021-06-13 16:46:59 +0800

    Add files via upload

---
+++ Debug/Core/Src/main.d
@@ -0,0 +1,231 @@
+Core/Src/main.o: ../Core/Src/main.cpp ../Core/Inc/main.h \
+ ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal.h \
+ ../Core/Inc/stm32f4xx_hal_conf.h \
+ ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc.h \
+ ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_def.h \
+ ../Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h \
+ ../Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f446xx.h \
+ ../Drivers/CMSIS/Include/core_cm4.h \
+ ../Drivers/CMSIS/Include/cmsis_version.h \
+ ../Drivers/CMSIS/Include/cmsis_compiler.h \
+ ../Drivers/CMSIS/Include/cmsis_gcc.h \
+ ../Drivers/CMSIS/Include/mpu_armv7.h \
+ ../Drivers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h \
+ ../Drivers/STM32F4xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h \
+ ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc_ex.h \
+ ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio.h \
+ ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio_ex.h \
+ ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_exti.h \
+ ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma.h \
+ ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma_ex.h \
+ ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_cortex.h \
+ ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_crc.h \
+ ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash.h \
+ ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ex.h \
+ ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ramfunc.h \
+ ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr.h \
+ ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr_ex.h \
+ ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_uart.h \
+ ../Middlewares/Third_Party/EdgeImpulse_CheongZhengQuan-project-1_MachineLearning/edgeimpulse/edge-impulse-sdk/classifier/ei_run_classifier.h \
+ ../Middlewares/Third_Party/EdgeImpulse_CheongZhengQuan-project-1_MachineLearning/edgeimpulse/model-parameters/model_metadata.h \
+ ../Middlewares/Third_Party/EdgeImpulse_CheongZhengQuan-project-1_MachineLearning/edgeimpulse/edge-impulse-sdk/classifier/ei_run_dsp.h \
+ ../Middlewares/Third_Party/EdgeImpulse_CheongZhengQuan-project-1_MachineLearning/edgeimpulse/edge-impulse-sdk/dsp/spectral/spectral.hpp \
+ ../Middlewares/Third_Party/EdgeImpulse_CheongZhengQuan-project-1_MachineLearning/edgeimpulse/edge-impulse-sdk/dsp/spectral/../config.hpp \
+ ../Middlewares/Third_Party/EdgeImpulse_CheongZhengQuan-project-1_MachineLearning/edgeimpulse/edge-impulse-sdk/dsp/spectral/processing.hpp \
+ ../Middlewares/Third_Party/EdgeImpulse_CheongZhengQuan-project-1_MachineLearning/edgeimpulse/edge-impulse-sdk/dsp/spectral/../numpy.hpp \
+ ../Middlewares/Third_Party/EdgeImpulse_CheongZhengQuan-project-1_MachineLearning/edgeimpulse/edge-impulse-sdk/dsp/spectral/../numpy_types.h \
+ ../Middlewares/Third_Party/EdgeImpulse_CheongZhengQuan-project-1_MachineLearning/edgeimpulse/edge-impulse-sdk/dsp/spectral/../config.hpp \
+ ../Middlewares/Third_Party/EdgeImpulse_CheongZhengQuan-project-1_MachineLearning/edgeimpulse/edge-impulse-sdk/dsp/spectral/../../porting/ei_classifier_porting.h \
+ ../Middlewares/Third_Party/EdgeImpulse_CheongZhengQuan-project-1_MachineLearning/edgeimpulse/edge-impulse-sdk/tensorflow/lite/micro/debug_log.h \
+ ../Middlewares/Third_Party/EdgeImpulse_CheongZhengQuan-project-1_MachineLearning/edgeimpulse/edge-impulse-sdk/dsp/spectral/../returntypes.hpp \
+ ../Middlewares/Third_Party/EdgeImpulse_CheongZhengQuan-project-1_MachineLearning/edgeimpulse/edge-impulse-sdk/dsp/spectral/../memory.hpp \
+ ../Middlewares/Third_Party/EdgeImpulse_CheongZhengQuan-project-1_MachineLearning/edgeimpulse/edge-impulse-sdk/dsp/spectral/../dct/fast-dct-fft.h \
+ ../Middlewares/Third_Party/EdgeImpulse_CheongZhengQuan-project-1_MachineLearning/edgeimpulse/edge-impulse-sdk/dsp/spectral/../dct/../kissfft/kiss_fft.h \
+ ../Middlewares/Third_Party/EdgeImpulse_CheongZhengQuan-project-1_MachineLearning/edgeimpulse/edge-impulse-sdk/dsp/spectral/../dct/../kissfft/../../porting/ei_classifier_porting.h \
+ ../Middlewares/Third_Party/EdgeImpulse_CheongZhengQuan-project-1_MachineLearning/edgeimpulse/edge-impulse-sdk/dsp/spectral/../kissfft/kiss_fftr.h \
+ ../Middlewares/Third_Party/EdgeImpulse_CheongZhengQuan-project-1_MachineLearning/edgeimpulse/edge-impulse-sdk/dsp/spectral/../kissfft/kiss_fft.h \
+ ../Middlewares/Third_Party/EdgeImpulse_CheongZhengQuan-project-1_MachineLearning/edgeimpulse/edge-impulse-sdk/CMSIS/DSP/Include/arm_math.h \
+ ../Middlewares/Third_Party/EdgeImpulse_CheongZhengQuan-project-1_MachineLearning/edgeimpulse/edge-impulse-sdk/CMSIS/Core/Include/cmsis_compiler.h \
+ ../Middlewares/Third_Party/EdgeImpulse_CheongZhengQuan-project-1_MachineLearning/edgeimpulse/edge-impulse-sdk/dsp/spectral/filters.hpp \
+ ../Middlewares/Third_Party/EdgeImpulse_CheongZhengQuan-project-1_MachineLearning/edgeimpulse/edge-impulse-sdk/dsp/spectral/fir_filter.hpp \
+ ../Middlewares/Third_Party/EdgeImpulse_CheongZhengQuan-project-1_MachineLearning/edgeimpulse/edge-impulse-sdk/dsp/spectral/feature.hpp \
+ ../Middlewares/Third_Party/EdgeImpulse_CheongZhengQuan-project-1_MachineLearning/edgeimpulse/edge-impulse-sdk/dsp/speechpy/speechpy.hpp \
+ ../Middlewares/Third_Party/EdgeImpulse_CheongZhengQuan-project-1_MachineLearning/edgeimpulse/edge-impulse-sdk/dsp/speechpy/../config.hpp \
+ ../Middlewares/Third_Party/EdgeImpulse_CheongZhengQuan-project-1_MachineLearning/edgeimpulse/edge-impulse-sdk/dsp/speechpy/feature.hpp \
+ ../Middlewares/Third_Party/EdgeImpulse_CheongZhengQuan-project-1_MachineLearning/edgeimpulse/edge-impulse-sdk/dsp/speechpy/functions.hpp \
+ ../Middlewares/Third_Party/EdgeImpulse_CheongZhengQuan-project-1_MachineLearning/edgeimpulse/edge-impulse-sdk/dsp/speechpy/../numpy.hpp \
+ ../Middlewares/Third_Party/EdgeImpulse_CheongZhengQuan-project-1_MachineLearning/edgeimpulse/edge-impulse-sdk/dsp/speechpy/../returntypes.hpp \
+ ../Middlewares/Third_Party/EdgeImpulse_CheongZhengQuan-project-1_MachineLearning/edgeimpulse/edge-impulse-sdk/dsp/speechpy/processing.hpp \
+ ../Middlewares/Third_Party/EdgeImpulse_CheongZhengQuan-project-1_MachineLearning/edgeimpulse/edge-impulse-sdk/dsp/speechpy/../memory.hpp \
+ ../Middlewares/Third_Party/EdgeImpulse_CheongZhengQuan-project-1_MachineLearning/edgeimpulse/edge-impulse-sdk/classifier/ei_classifier_types.h \
+ ../Middlewares/Third_Party/EdgeImpulse_CheongZhengQuan-project-1_MachineLearning/edgeimpulse/edge-impulse-sdk/classifier/ei_classifier_smooth.h \
+ ../Middlewares/Third_Party/EdgeImpulse_CheongZhengQuan-project-1_MachineLearning/edgeimpulse/edge-impulse-sdk/classifier/ei_signal_with_axes.h \
+ ../Middlewares/Third_Party/EdgeImpulse_CheongZhengQuan-project-1_MachineLearning/edgeimpulse/edge-impulse-sdk/dsp/numpy_types.h \
+ ../Middlewares/Third_Party/EdgeImpulse_CheongZhengQuan-project-1_MachineLearning/edgeimpulse/edge-impulse-sdk/dsp/returntypes.hpp \
+ ../Middlewares/Third_Party/EdgeImpulse_CheongZhengQuan-project-1_MachineLearning/edgeimpulse/edge-impulse-sdk/porting/ei_classifier_porting.h \
+ ../Middlewares/Third_Party/EdgeImpulse_CheongZhengQuan-project-1_MachineLearning/edgeimpulse/model-parameters/dsp_blocks.h \
+ ../Middlewares/Third_Party/EdgeImpulse_CheongZhengQuan-project-1_MachineLearning/edgeimpulse/edge-impulse-sdk/classifier/ei_run_dsp.h \
+ ../Middlewares/Third_Party/EdgeImpulse_CheongZhengQuan-project-1_MachineLearning/edgeimpulse/edge-impulse-sdk/classifier/ei_model_types.h \
+ ../Middlewares/Third_Party/EdgeImpulse_CheongZhengQuan-project-1_MachineLearning/edgeimpulse/edge-impulse-sdk/dsp/numpy.hpp \
+ ../Middlewares/Third_Party/EdgeImpulse_CheongZhengQuan-project-1_MachineLearning/edgeimpulse/edge-impulse-sdk/tensorflow/lite/c/common.h \
+ ../Middlewares/Third_Party/EdgeImpulse_CheongZhengQuan-project-1_MachineLearning/edgeimpulse/edge-impulse-sdk/tensorflow/lite/kernels/internal/tensor_ctypes.h \
+ ../Middlewares/Third_Party/EdgeImpulse_CheongZhengQuan-project-1_MachineLearning/edgeimpulse/edge-impulse-sdk/tensorflow/lite/kernels/internal/types.h \
+ ../Middlewares/Third_Party/EdgeImpulse_CheongZhengQuan-project-1_MachineLearning/edgeimpulse/edge-impulse-sdk/tensorflow/lite/kernels/internal/compatibility.h \
+ ../Middlewares/Third_Party/EdgeImpulse_CheongZhengQuan-project-1_MachineLearning/edgeimpulse/edge-impulse-sdk/tensorflow/lite/kernels/op_macros.h \
+ ../Middlewares/Third_Party/EdgeImpulse_CheongZhengQuan-project-1_MachineLearning/edgeimpulse/tflite-model/trained_model_compiled.h \
+ ../Middlewares/Third_Party/EdgeImpulse_CheongZhengQuan-project-1_MachineLearning/edgeimpulse/edge-impulse-sdk/classifier/ei_aligned_malloc.h \
+ ../Middlewares/Third_Party/EdgeImpulse_CheongZhengQuan-project-1_MachineLearning/edgeimpulse/edge-impulse-sdk/classifier/../porting/ei_classifier_porting.h
+
+../Core/Inc/main.h:
+
+../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal.h:
+
+../Core/Inc/stm32f4xx_hal_conf.h:
+
+../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc.h:
+
+../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_def.h:
+
+../Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h:
+
+../Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f446xx.h:
+
+../Drivers/CMSIS/Include/core_cm4.h:
+
+../Drivers/CMSIS/Include/cmsis_version.h:
+
+../Drivers/CMSIS/Include/cmsis_compiler.h:
+
+../Drivers/CMSIS/Include/cmsis_gcc.h:
+
+../Drivers/CMSIS/Include/mpu_armv7.h:
+
+../Drivers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h:
+
+../Drivers/STM32F4xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h:
+
+../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc_ex.h:
+
+../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio.h:
+
+../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio_ex.h:
+
+../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_exti.h:
+
+../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma.h:
+
+../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma_ex.h:
+
+../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_cortex.h:
+
+../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_crc.h:
+
+../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash.h:
+
+../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ex.h:
+
+../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ramfunc.h:
+
+../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr.h:
+
+../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr_ex.h:
+
+../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_uart.h:
+
+../Middlewares/Third_Party/EdgeImpulse_CheongZhengQuan-project-1_MachineLearning/edgeimpulse/edge-impulse-sdk/classifier/ei_run_classifier.h:
+
+../Middlewares/Third_Party/EdgeImpulse_CheongZhengQuan-project-1_MachineLearning/edgeimpulse/model-parameters/model_metadata.h:
+
+../Middlewares/Third_Party/EdgeImpulse_CheongZhengQuan-project-1_MachineLearning/edgeimpulse/edge-impulse-sdk/classifier/ei_run_dsp.h:
+
+../Middlewares/Third_Party/EdgeImpulse_CheongZhengQuan-project-1_MachineLearning/edgeimpulse/edge-impulse-sdk/dsp/spectral/spectral.hpp:
+
+../Middlewares/Third_Party/EdgeImpulse_CheongZhengQuan-project-1_MachineLearning/edgeimpulse/edge-impulse-sdk/dsp/spectral/../config.hpp:
+
+../Middlewares/Third_Party/EdgeImpulse_CheongZhengQuan-project-1_MachineLearning/edgeimpulse/edge-impulse-sdk/dsp/spectral/processing.hpp:
+
+../Middlewares/Third_Party/EdgeImpulse_CheongZhengQuan-project-1_MachineLearning/edgeimpulse/edge-impulse-sdk/dsp/spectral/../numpy.hpp:
+
+../Middlewares/Third_Party/EdgeImpulse_CheongZhengQuan-project-1_MachineLearning/edgeimpulse/edge-impulse-sdk/dsp/spectral/../numpy_types.h:
+
+../Middlewares/Third_Party/EdgeImpulse_CheongZhengQuan-project-1_MachineLearning/edgeimpulse/edge-impulse-sdk/dsp/spectral/../config.hpp:
+
+../Middlewares/Third_Party/EdgeImpulse_CheongZhengQuan-project-1_MachineLearning/edgeimpulse/edge-impulse-sdk/dsp/spectral/../../porting/ei_classifier_porting.h:
+
+../Middlewares/Third_Party/EdgeImpulse_CheongZhengQuan-project-1_MachineLearning/edgeimpulse/edge-impulse-sdk/tensorflow/lite/micro/debug_log.h:
+
+../Middlewares/Third_Party/EdgeImpulse_CheongZhengQuan-project-1_MachineLearning/edgeimpulse/edge-impulse-sdk/dsp/spectral/../returntypes.hpp:
+
+../Middlewares/Third_Party/EdgeImpulse_CheongZhengQuan-project-1_MachineLearning/edgeimpulse/edge-impulse-sdk/dsp/spectral/../memory.hpp:
+
+../Middlewares/Third_Party/EdgeImpulse_CheongZhengQuan-project-1_MachineLearning/edgeimpulse/edge-impulse-sdk/dsp/spectral/../dct/fast-dct-fft.h:
+
+../Middlewares/Third_Party/EdgeImpulse_CheongZhengQuan-project-1_MachineLearning/edgeimpulse/edge-impulse-sdk/dsp/spectral/../dct/../kissfft/kiss_fft.h:
+
+../Middlewares/Third_Party/EdgeImpulse_CheongZhengQuan-project-1_MachineLearning/edgeimpulse/edge-impulse-sdk/dsp/spectral/../dct/../kissfft/../../porting/ei_classifier_porting.h:
+
+../Middlewares/Third_Party/EdgeImpulse_CheongZhengQuan-project-1_MachineLearning/edgeimpulse/edge-impulse-sdk/dsp/spectral/../kissfft/kiss_fftr.h:
+
+../Middlewares/Third_Party/EdgeImpulse_CheongZhengQuan-project-1_MachineLearning/edgeimpulse/edge-impulse-sdk/dsp/spectral/../kissfft/kiss_fft.h:
+
+../Middlewares/Third_Party/EdgeImpulse_CheongZhengQuan-project-1_MachineLearning/edgeimpulse/edge-impulse-sdk/CMSIS/DSP/Include/arm_math.h:
+
+../Middlewares/Third_Party/EdgeImpulse_CheongZhengQuan-project-1_MachineLearning/edgeimpulse/edge-impulse-sdk/CMSIS/Core/Include/cmsis_compiler.h:
+
+../Middlewares/Third_Party/EdgeImpulse_CheongZhengQuan-project-1_MachineLearning/edgeimpulse/edge-impulse-sdk/dsp/spectral/filters.hpp:
+
+../Middlewares/Third_Party/EdgeImpulse_CheongZhengQuan-project-1_MachineLearning/edgeimpulse/edge-impulse-sdk/dsp/spectral/fir_filter.hpp:
+
+../Middlewares/Third_Party/EdgeImpulse_CheongZhengQuan-project-1_MachineLearning/edgeimpulse/edge-impulse-sdk/dsp/spectral/feature.hpp:
+
+../Middlewares/Third_Party/EdgeImpulse_CheongZhengQuan-project-1_MachineLearning/edgeimpulse/edge-impulse-sdk/dsp/speechpy/speechpy.hpp:
+
+../Middlewares/Third_Party/EdgeImpulse_CheongZhengQuan-project-1_MachineLearning/edgeimpulse/edge-impulse-sdk/dsp/speechpy/../config.hpp:
+
+../Middlewares/Third_Party/EdgeImpulse_CheongZhengQuan-project-1_MachineLearning/edgeimpulse/edge-impulse-sdk/dsp/speechpy/feature.hpp:
+
+../Middlewares/Third_Party/EdgeImpulse_CheongZhengQuan-project-1_MachineLearning/edgeimpulse/edge-impulse-sdk/dsp/speechpy/functions.hpp:
+
+../Middlewares/Third_Party/EdgeImpulse_CheongZhengQuan-project-1_MachineLearning/edgeimpulse/edge-impulse-sdk/dsp/speechpy/../numpy.hpp:
+
+../Middlewares/Third_Party/EdgeImpulse_CheongZhengQuan-project-1_MachineLearning/edgeimpulse/edge-impulse-sdk/dsp/speechpy/../returntypes.hpp:
+
+../Middlewares/Third_Party/EdgeImpulse_CheongZhengQuan-project-1_MachineLearning/edgeimpulse/edge-impulse-sdk/dsp/speechpy/processing.hpp:
+
+../Middlewares/Third_Party/EdgeImpulse_CheongZhengQuan-project-1_MachineLearning/edgeimpulse/edge-impulse-sdk/dsp/speechpy/../memory.hpp:
+
+../Middlewares/Third_Party/EdgeImpulse_CheongZhengQuan-project-1_MachineLearning/edgeimpulse/edge-impulse-sdk/classifier/ei_classifier_types.h:
+
+../Middlewares/Third_Party/EdgeImpulse_CheongZhengQuan-project-1_MachineLearning/edgeimpulse/edge-impulse-sdk/classifier/ei_classifier_smooth.h:
+
+../Middlewares/Third_Party/EdgeImpulse_CheongZhengQuan-project-1_MachineLearning/edgeimpulse/edge-impulse-sdk/classifier/ei_signal_with_axes.h:
+
+../Middlewares/Third_Party/EdgeImpulse_CheongZhengQuan-project-1_MachineLearning/edgeimpulse/edge-impulse-sdk/dsp/numpy_types.h:
+
+../Middlewares/Third_Party/EdgeImpulse_CheongZhengQuan-project-1_MachineLearning/edgeimpulse/edge-impulse-sdk/dsp/returntypes.hpp:
+
+../Middlewares/Third_Party/EdgeImpulse_CheongZhengQuan-project-1_MachineLearning/edgeimpulse/edge-impulse-sdk/porting/ei_classifier_porting.h:
+
+../Middlewares/Third_Party/EdgeImpulse_CheongZhengQuan-project-1_MachineLearning/edgeimpulse/model-parameters/dsp_blocks.h:
+
+../Middlewares/Third_Party/EdgeImpulse_CheongZhengQuan-project-1_MachineLearning/edgeimpulse/edge-impulse-sdk/classifier/ei_run_dsp.h:
+
+../Middlewares/Third_Party/EdgeImpulse_CheongZhengQuan-project-1_MachineLearning/edgeimpulse/edge-impulse-sdk/classifier/ei_model_types.h:
+
+../Middlewares/Third_Party/EdgeImpulse_CheongZhengQuan-project-1_MachineLearning/edgeimpulse/edge-impulse-sdk/dsp/numpy.hpp:
+
+../Middlewares/Third_Party/EdgeImpulse_CheongZhengQuan-project-1_MachineLearning/edgeimpulse/edge-impulse-sdk/tensorflow/lite/c/common.h:
+
+../Middlewares/Third_Party/EdgeImpulse_CheongZhengQuan-project-1_MachineLearning/edgeimpulse/edge-impulse-sdk/tensorflow/lite/kernels/internal/tensor_ctypes.h:
+
+../Middlewares/Third_Party/EdgeImpulse_CheongZhengQuan-project-1_MachineLearning/edgeimpulse/edge-impulse-sdk/tensorflow/lite/kernels/internal/types.h:
+
+../Middlewares/Third_Party/EdgeImpulse_CheongZhengQuan-project-1_MachineLearning/edgeimpulse/edge-impulse-sdk/tensorflow/lite/kernels/internal/compatibility.h:
+
+../Middlewares/Third_Party/EdgeImpulse_CheongZhengQuan-project-1_MachineLearning/edgeimpulse/edge-impulse-sdk/tensorflow/lite/kernels/op_macros.h:
+
+../Middlewares/Third_Party/EdgeImpulse_CheongZhengQuan-project-1_MachineLearning/edgeimpulse/tflite-model/trained_model_compiled.h:
+
+../Middlewares/Third_Party/EdgeImpulse_CheongZhengQuan-project-1_MachineLearning/edgeimpulse/edge-impulse-sdk/classifier/ei_aligned_malloc.h:
+
+../Middlewares/Third_Party/EdgeImpulse_CheongZhengQuan-project-1_MachineLearning/edgeimpulse/edge-impulse-sdk/classifier/../porting/ei_classifier_porting.h:
